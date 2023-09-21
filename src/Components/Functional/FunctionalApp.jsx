e552b747e20f6113f4caf00cd430acf12fc202f3
Rogelio Servin <manchaster1509@gmail.com>
2023-09-20 20:58:57 -0500

    first commit

--- src/Components/Functional/FunctionalApp.jsx
+++ src/Components/Functional/FunctionalApp.jsx
@@ -1,13 +1,72 @@
+import { useState } from "react";
 import { FunctionalGameBoard } from "./FunctionalGameBoard";
 import { FunctionalScoreBoard } from "./FunctionalScoreBoard";
 import { FunctionalFinalScore } from "./FunctionalFinalScore";
+import { Images } from "../../assets/Images";
+
+const initialFishes = [
+  {
+    name: "trout",
+    url: Images.trout,
+  },
+  {
+    name: "salmon",
+    url: Images.salmon,
+  },
+  {
+    name: "tuna",
+    url: Images.tuna,
+  },
+  {
+    name: "shark",
+    url: Images.shark,
+  },
+];
+
+let correctCount = 0;
+let incorrectCount = 0;
 
 export function FunctionalApp() {
+  const [userInput, setUserInput] = useState("");
+  const [availableFish, setAvailableFish] = useState(initialFishes);
+
+  const nextFishToName = availableFish[0];
+
+  const removeFish = () => {
+    setAvailableFish((prevFish) =>
+      prevFish.filter((fish) => fish.name !== nextFishToName.name)
+    );
+  };
+
+  const handleAnswer = () => {
+    userInput === nextFishToName.name ? correctCount++ : incorrectCount++;
+  };
+
   return (
     <>
-      <FunctionalScoreBoard />
-      <FunctionalGameBoard />
-      {false && <FunctionalFinalScore />}
+      {availableFish.length ? (
+        <>
+          <FunctionalScoreBoard
+            correctCount={correctCount}
+            incorrectCount={incorrectCount}
+            handleAnswer={handleAnswer}
+            nextFishToName={nextFishToName}
+            availableFish={availableFish}
+          />
+          <FunctionalGameBoard
+            handleAnswer={handleAnswer}
+            nextFishToName={nextFishToName}
+            userInput={userInput}
+            setUserInput={setUserInput}
+            removeFish={removeFish}
+          />
+        </>
+      ) : (
+        <FunctionalFinalScore
+          correctCount={correctCount}
+          totalCount={initialFishes.length}
+        />
+      )}
     </>
   );
 }
--- src/Components/Functional/FunctionalGameBoard.jsx
+++ src/Components/Functional/FunctionalGameBoard.jsx
@@ -1,37 +1,33 @@
 import "./styles/game-board.css";
-import { Images } from "../../assets/Images";
 
-const initialFishes = [
-  {
-    name: "trout",
-    url: Images.trout,
-  },
-  {
-    name: "salmon",
-    url: Images.salmon,
-  },
-  {
-    name: "tuna",
-    url: Images.tuna,
-  },
-  {
-    name: "shark",
-    url: Images.shark,
-  },
-];
-
-export function FunctionalGameBoard() {
-  const nextFishToName = initialFishes[0];
+export const FunctionalGameBoard = ({
+  setUserInput,
+  userInput,
+  nextFishToName,
+  removeFish,
+  handleAnswer,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAnswer();
+    removeFish();
+    setUserInput("");
+  };
   return (
     <div id="game-board">
       <div id="fish-container">
         <img src={nextFishToName.url} alt={nextFishToName.name} />
       </div>
-      <form id="fish-guess-form">
+      <form onSubmit={handleSubmit} id="fish-guess-form">
         <label htmlFor="fish-guess">What kind of fish is this?</label>
-        <input type="text" name="fish-guess" />
+        <input
+          value={userInput}
+          onChange={(event) => setUserInput(event.target.value)}
+          type="text"
+          name="fish-guess"
+        />
         <input type="submit" />
       </form>
     </div>
   );
-}
+};
